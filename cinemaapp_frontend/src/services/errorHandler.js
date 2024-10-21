f3f6afbf7b6466f093b21d1001834e657e39cf3c
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-10-21 14:33:26 +0200

    Removing console.error and Toast notificaiton out of every block

--- cinemaapp_frontend/src/services/errorHandler.js
+++ cinemaapp_frontend/src/services/errorHandler.js
@@ -5,17 +5,17 @@ class ErrorHandler {
     let errorMsg = "An unknown error occurred.";
 
     if (error.response) {
-      errorMsg = error.response.data.message || errorMsg;
-      console.error(`Error: ${error.response.status} - ${errorMsg}`)
-      ToastService.error(`Error: ${error.response.status} - ${errorMsg}`);
+      errorMsg = `Error: ${error.response.status} - ${
+        error.response.data.message || errorMsg
+      }`;
     } else if (error.request) {
-      errorMsg = "No response received back from the server.";
-      ToastService.error(`Error: ${errorMsg} | ",${error.request}`);
-      console.error(`Error: ${errorMsg} | ",${error.request}`)
+      errorMsg = `Error: No response received | ", ${error.request}`;
     } else {
-      ToastService.error("Error: ", error.message);
-      console.error("Error: ", error.message)
+      errorMsg = `Error: ${error.message}`;
     }
+
+    console.error(`Error: ${error.response.status} - ${errorMsg}`);
+    ToastService.error(`Error: ${error.response.status} - ${errorMsg}`);
   }
 }
 
