603068bb3b89dbf70974cd3c1f2701866daa0009
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-10-21 17:35:56 +0200

    Adding finishing touches

--- cinemaapp_frontend/src/components/Card.jsx
+++ cinemaapp_frontend/src/components/Card.jsx
@@ -4,8 +4,8 @@ import "../styles/Card.css";
 const Card = ({ title, duration, genre, imageUrl }) => {
   return (
     <div className="rounded-5 p-3 border shadow">
-      <div className="rounded-5"/>
-      <img src={imageUrl} className="card"/>
+      <div className="rounded-5" />
+      <img src={imageUrl} className="card" />
       <h5 className="mt-2 fw-bold">{title}</h5>
       <h6>
         {duration} min| {genre}
--- cinemaapp_frontend/src/components/PaginatedList.jsx
+++ cinemaapp_frontend/src/components/PaginatedList.jsx
@@ -3,31 +3,10 @@ import Card from "./Card";
 import { Pagination } from "react-bootstrap";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import SmallButton from "./SmallButton";
-import "../styles/PaginatedList.css"; // Assuming the CSS file
 
 const PaginatedList = ({ title, data }) => {
+  const [itemsPerPage, setItemsPerPage] = useState(4); // Default to 4 items per page
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(4);
-
-  useEffect(() => {
-    const updateItemsPerPage = () => {
-      if (window.matchMedia("(max-width: 576px)").matches) {
-        setItemsPerPage(1);
-      } else if (window.matchMedia("(max-width: 768px)").matches) {
-        setItemsPerPage(2);
-      } else if (window.matchMedia("(max-width: 1600px)").matches) {
-        setItemsPerPage(4);
-      } else {
-        setItemsPerPage(6);
-      }
-    };
-
-    updateItemsPerPage();
-
-    window.addEventListener("resize", updateItemsPerPage);
-
-    return () => window.removeEventListener("resize", updateItemsPerPage);
-  }, []);
 
   const indexOfLastCard = currentPage * itemsPerPage;
   const indexOfFirstCard = indexOfLastCard - itemsPerPage;
@@ -47,27 +26,43 @@ const PaginatedList = ({ title, data }) => {
     }
   };
 
+  useEffect(() => {
+    const updateItemsPerPage = () => {
+      if (window.innerWidth <= 991) {
+        setItemsPerPage(1);
+      } else {
+        setItemsPerPage(4);
+      }
+    };
+
+    updateItemsPerPage();
+    window.addEventListener("resize", updateItemsPerPage);
+
+    return () => window.removeEventListener("resize", updateItemsPerPage);
+  }, []);
+
   return (
-    <div className="paginated-list-container">
-      <div className="paginated-list-header">
-        <h3 className="paginated-list-title">{title}</h3>
-        <h6 className="paginated-list-see-all">See all</h6>
+    <div style={{ padding: "3rem 4rem" }}>
+      <div className="d-flex justify-content-between align-items-center">
+        <h3 className="mb-4 fw-bold">{title}</h3>
+        <h6 className="fw-bold primary-red">See all</h6>
       </div>
 
-      <div className="paginated-list-cards">
+      <div className="row">
         {currentCards.map((card) => (
-          <Card
-            key={card.id}
-            title={card.title}
-            duration={card.duration}
-            genre={card.genre.join(", ")}
-            imageUrl={card.image}
-          />
+          <div className="col-12 col-lg-3 col-lg-3" key={card.id}>
+            <Card
+              title={card.title}
+              duration={card.duration}
+              genre={card.genre.join(", ")}
+              imageUrl={card.image}
+            />
+          </div>
         ))}
       </div>
 
-      <div className="paginated-list-footer">
-        <div className="paginated-list-info">
+      <div className="d-flex flex-column flex-md-row justify-content-end align-items-center mt-4 pl-4">
+        <div className="text-center mb-3 my-auto mx-4">
           <h6>
             Showing{" "}
             <span className="fw-bold">
@@ -76,7 +71,7 @@ const PaginatedList = ({ title, data }) => {
             out of <span className="fw-bold">{data.length}</span>
           </h6>
         </div>
-        <Pagination className="paginated-list-pagination">
+        <Pagination className="gap-4 mt-2">
           <SmallButton
             onClick={handlePreviousPage}
             icon={<FaArrowLeft />}
