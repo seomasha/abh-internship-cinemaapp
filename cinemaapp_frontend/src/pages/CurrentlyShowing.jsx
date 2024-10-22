732ca94d2b71a3756f6d08fa61c1abef779ab39c
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-10-22 10:10:32 +0200

    Adding dropdown component

--- cinemaapp_frontend/src/pages/CurrentlyShowing.jsx
+++ cinemaapp_frontend/src/pages/CurrentlyShowing.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
 import SearchBar from "../components/SearchBar";
+import Dropdown from "../components/Dropdown";
+
+import { CiLocationOn, CiClock1 } from "react-icons/ci";
 
 const CurrentlyShowing = () => {
   return (
@@ -12,6 +15,36 @@ const CurrentlyShowing = () => {
         <h2 className="fw-bold">Currently showing (9)</h2>
 
         <SearchBar />
+        <div className="row gx-4">
+          <div className="col-12 col-md-3">
+            <Dropdown
+              icon={CiLocationOn}
+              title="All Cities"
+              options={["Option 1", "Option 2"]}
+            />
+          </div>
+          <div className="col-12 col-md-3">
+            <Dropdown
+              icon={CiLocationOn}
+              title="All Cinemas"
+              options={["Option 1", "Option 2"]}
+            />
+          </div>
+          <div className="col-12 col-md-3">
+            <Dropdown
+              icon={CiLocationOn}
+              title="All Genres"
+              options={["Option 1", "Option 2"]}
+            />
+          </div>
+          <div className="col-12 col-md-3">
+            <Dropdown
+              icon={CiClock1}
+              title="All Projection Times"
+              options={["Option 1", "Option 2"]}
+            />
+          </div>
+        </div>
       </div>
 
       <Footer />
