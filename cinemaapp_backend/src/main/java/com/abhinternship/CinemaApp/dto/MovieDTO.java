4abc0056ca0b2358718b8389b61c385f74638d56
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-10-30 12:19:35 +0100

    Adding paginated venues

--- cinemaapp_backend/src/main/java/com/abhinternship/CinemaApp/dto/MovieDTO.java
+++ cinemaapp_backend/src/main/java/com/abhinternship/CinemaApp/dto/MovieDTO.java
@@ -6,11 +6,11 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class MoviePageResponse {
-    private List<Movie> movies;
-    private long totalSize;
+public class MoviePageResponse { // Rename to MovieDTO
+    private final List<Movie> movies;
+    private final long totalSize;
 
-    public MoviePageResponse(List<Movie> movies, long totalSize) {
+    public MoviePageResponse(final List<Movie> movies, final long totalSize) {
         this.movies = movies;
         this.totalSize = totalSize;
     }
