8b763a9c224341eb06fb66780211a5970f322f1f
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-11-04 14:51:41 +0100

    Slight frontend modifications

--- cinemaapp_frontend/src/components/Card.jsx
+++ cinemaapp_frontend/src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ title, subtitle, genre, imageUrl }) => {
   return (
     <div className="rounded-5 p-3 border shadow card-container">
       <div className="rounded-5" />
-      <img src={imageUrl} className="card" />
+      <img src={imageUrl} className="card-image" />
       <h5 className="mt-2 fw-bold">{title}</h5>
       <h6>
         {subtitle}
--- cinemaapp_frontend/src/components/VenueButton.jsx
+++ cinemaapp_frontend/src/components/VenueButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "../styles/VenueButton.css";
 
-const VenueButton = ({ id, name, onClick }) => {
+const VenueButton = ({ id, name, onClick, isSelected }) => {
   return (
-    <div className="venue-button">
+    <div className={`venue-button ${isSelected ? "selected" : ""}`}>
       <h5 key={id} onClick={() => onClick(id)}>
         {name}
       </h5>
