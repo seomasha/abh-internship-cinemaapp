373afb381dfeb4ae9704e414e0c2e29024adb192
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-12-10 14:24:30 +0100

    Adding bugfix (#25)

--- cinemaapp_frontend/src/pages/AboutUs.jsx
+++ cinemaapp_frontend/src/pages/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import "../styles/AboutUs.css";
 
